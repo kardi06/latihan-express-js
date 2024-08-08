45d3be4ffcd44aad465eadc4835a0d016b77eae9
Kardi <kgland06@gmail.com>
2024-08-08 13:11:35 +0700

    response json and sendfile

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
 import express from "express";
+import url from "url";
 
 const app = express();
 const port = 3000;
 
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 app.get("/",(req,res)=>{
-    res.send("Hello World!");
+    // res.send("Hello World!");
+    // res.json({
+    //     name: "Hello World!",
+    //     age:21
+    // });
+    console.log(__dirname);
+    res.sendFile("./page/index.html",{root:__dirname});
 });
 
 app.get("/about",(req,res)=>{
-    res.send("About Page");
+    // res.send("About Page");
+    res.sendFile("./page/about.html",{root:__dirname});
 });
 
 app.get("/contact",(req,res)=>{
-    res.send("Contact Page");
+    // res.send("Contact Page");
+    res.sendFile("./page/contact.html",{root:__dirname});
 });
 
 app.use("*",(req,res)=>{
